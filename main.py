252babdc8b9ab2619d890fe96df25d5eacbb9034
Mateus Schverz <matefs8569@gmail.com>
2024-08-26 08:56:08 -0300

    feat(cors): add cors from any origin

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from duckduckgo_search import DDGS
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"] 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
 
 @app.get("/chat/")
 async def chat(query: str):
