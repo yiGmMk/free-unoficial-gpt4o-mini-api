d44b6bd9d85717149765c352cfa7087819aaaeca
Mateus Schverz <matefs8569@gmail.com>
2024-09-03 23:32:23 -0300

    Create requests-poc.sh

---
+++ requests-poc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for ((i=1; i<=10000; i++))
+do
+  echo "Iteration $i"
+  curl --location 'http://127.0.0.1:9090/chat/?query=contexto%3A%20voce%20esta%20lendo%20macro%20de%20um%20motorista%20monitorado%2C%20qual%20evento%20(EM%20DIRECAO%2C%20EM%20DESCANSO%2C%20EM%20REFEICAO%2C%20EM%20REPOUSO%2C%20EM%20ESPERA%2C%20PARADO%20EM%20JORNADA%2C%20EMBARQUE%20DE%20MOTORISTA%2C%20INICIO%20DE%20DIRECAO%2C%20FIM%20DE%20DIRECAO%2C%20DESEMBARQUE%20DE%20MOTORISTA%2C%20SEM%20POSICAO%20)%20est%C3%A1%20associado%20a%20descri%C3%A7%C3%A3o%20a%20seguir%3A%20esqueci%20de%20bater%20volta%20do%20almo%C3%A7o'
+done
